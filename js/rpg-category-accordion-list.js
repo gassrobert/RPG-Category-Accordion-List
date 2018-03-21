de266ca05b884937c4b755feca4c0dc9d9125b05
gassrobertpaul <gassrobertpaul@yahoo.com>
2018-03-21 11:57:20 -0700

    add anchor tag conditional statement to the click event

--- js/rpg-category-accordion-list.js
+++ js/rpg-category-accordion-list.js
@@ -1,12 +1,16 @@
 jQuery(document).ready(function($) {
 	// When visitor clicks on Accordion Heading it opens the submenu
-	$(document).on('click','.rpgAccordionHeading', function(){
+	$(document).on('click','.rpgAccordionHeading', function(e){
+
+		// Get the id of the clicked element, take out the number, and apply the number to the submenu
 		var clickedID = $(this).attr("id");
 		currentID = clickedID.replace("AccID", "");
 		var SubMenuID = 'subMenu'  + currentID;
-		var isHidden = document.getElementById(SubMenuID).style.display == "none"; 
-		// if (isHidden) {
+
+		// If the Accordion Heading is clicked on  without clicking the anchor tag, then activate the slide toggle
+		if(!$(e.target).is("a")) { 
 			$( "#"+SubMenuID ).slideToggle("fast");
-		// }	
+		}
+
 	}); // End of $(document).on('click','.rpgAccordionHeading', function(){ 
 }); // End of jQuery(document).ready(function($) {
